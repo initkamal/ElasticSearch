98148da1ad154b2913d5dacb6b6cbcb878f47135
Kamal Singh Yadav <intgrl.kamal08@gmail.com>
2021-06-04 14:02:43 +0530

    Initial commit of es-multinode-cluster

---
+++ es-multinode-cluster/main.tf
@@ -0,0 +1,91 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_security_group" "security_group" {
+  name        = var.security_group
+  description = "Allow TLS inbound traffic"
+
+
+  dynamic "ingress" {
+    for_each = var.security_group_ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = {
+    Name = var.security_group
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
+
+resource "aws_key_pair" "elasticsearch_deployer_key" {
+  key_name   = var.key_pair
+  public_key = file("${path.module}/ssh-key/id_rsa.pub")
+
+}
+
+resource "aws_instance" "elasticsearch_instance" {
+
+  count = var.ec_count
+
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.instance_type
+  security_groups = ["${var.security_group}"]
+  key_name        = aws_key_pair.elasticsearch_deployer_key.id
+
+  tags = {
+    Name = "${var.elasticsearch_instance} ${count.index}"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt upgrade -y", "sudo apt install python3 -y", "echo Bootstrapping of the instances done!"]
+
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${path.module}/ssh-key/id_rsa")
+    }
+  }
+
+  #provisioner "local-exec" {
+  #  command = "${count.index}" == 0 ? "echo ${self.public_ip} is_master=true >> hosts" : "echo ${self.public_ip} is_master=false >> hosts"
+  #}
+
+}
+
+resource "null_resource" "ansible-running" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i inventory --private-key ssh-key/id_rsa  es_provisioning.yml"
+
+  }
+}
---
+++ es-multinode-cluster/variables.tf
@@ -0,0 +1,36 @@
+variable "region" {
+  default = "us-east-1"
+
+}
+
+variable "instance_type" {
+  default = "t3.small"
+
+}
+
+variable "elasticsearch_instance" {
+  default = "ElasticSearch Instance"
+
+}
+
+variable "ec_count" {
+  default = "2"
+
+}
+
+variable "security_group" {
+  default = "ElasticSearch SG"
+
+}
+
+variable "security_group_ports" {
+  type        = list(number)
+  description = "List of Ingress ports"
+  default     = [22, 80, 443, 9200, 9300, 5601]
+
+}
+
+variable "key_pair" {
+  default = "elasticsearch-deployer-key"
+
+}
